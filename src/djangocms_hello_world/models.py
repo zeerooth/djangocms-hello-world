42e4e6689f5628bd4c6313a3ca3551ab2d35cde7
Radosław Stępień <rstepien@cloudferro.com>
2020-04-03 15:28:59 +0200

    pep8 changes + ci settings module fix

--- src/djangocms_hello_world/models.py
+++ src/djangocms_hello_world/models.py
@@ -2,8 +2,9 @@ from cms.models.pluginmodel import CMSPlugin
 from django.db import models
 
 
-class HelloWorldPlugin(CMSPlugin): # pylint: disable=model-no-explicit-unicode
-    text: models.TextField = models.TextField("Your custom text", null=True, blank=True)
+class HelloWorldPlugin(CMSPlugin):  # pylint: disable=model-no-explicit-unicode
+    text: models.TextField = models.TextField("Your custom text", 
+                                              null=True, blank=True)
 
     class Meta:
         verbose_name = "Hello world plugin"
